f8f4fc467983ff1df2ba6d9d09dcb71fdb9254bf
unknown <rayen.frih.pro@mail.com>
2022-01-21 17:31:33 +0100

    test

---
+++ webservice1/src/main/java/com/example/webservice1/RestWebService.java
@@ -0,0 +1,18 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebServiceExample {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello !";
+    
+    public setMapping(){
+        this.Mapping = new this.Mapping;
+    }
+
+    public getMapping(){
+        return this.Mapping;
+    }
+}
+}
